7a682f8d84241c8c7f3dd51d80c5e782deba6054
Yongze <sapphire.php@gmail.com>
2018-01-10 17:17:48 +0800

    update

--- application/database-sample.php
+++ application/database-sample.php
@@ -18,11 +18,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'think_pro',
+    'database'        => 'think.admin',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'think.admin',
     // 密码
-    'password'        => 'ba190035',
+    'password'        => 'think.admin',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
