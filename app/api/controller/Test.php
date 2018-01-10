8f2d385d6bb2856d78303b73c149d853bd654099
Yongze <sapphire.php@gmail.com>
2018-01-10 18:25:32 +0800

    接口问题

---
+++ app/api/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by sapphire.php@gmail.com
+ * User: yongze
+ * Date: 2018/1/10
+ * Time: 下午6:22
+ */
+namespace app\api\controller;
+
+use think\Controller;
+
+class Test extends Controller{
+
+    public function j()
+    {
+        echo "x";
+    }
+}
